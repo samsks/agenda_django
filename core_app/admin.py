1c813701931821ab8512c49077b41b8f48fa7c32
SamSKS <samuel.meireles.15s@outlook.com>
2023-02-09 00:02:22 -0300

    add: create table events

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from core_app.models import Event
+
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'create_date')
+    list_filter = ('user', 'event_date')
+
+
+admin.site.register(Event, EventAdmin)
--- core_app/models.py
+++ core_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now=True, verbose_name='Registered in')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'events'
+
+    def __str__(self):
+        return self.title
+
+
